668945aea71a69f65e4cf3eacce478982d84c88b
ubikatcdmx <ubikatcdmx@gmail.com>
2025-11-30 19:05:38 -0600

    script_intro.js

--- js/script_intro.js
+++ js/script_intro.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Redirige después de 5 segundos (5000 milisegundos)
     setTimeout(() => {
-        window.location.href = '/views/menu.html'; // Cambia 'menu.html' por la URL de tu menú
+        window.location.href = 'views/menu.html'; // Cambia 'menu.html' por la URL de tu menú
     }, 5000);
+
 });
