138654aaf5ae748714aa504d60ccbdc61c58ccb3
ubikatcdmx <ubikatcdmx@gmail.com>
2025-11-30 22:48:46 -0600

    Delete js/script_intro.js

--- js/script_intro.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Redirige después de 5 segundos (5000 milisegundos)
-    setTimeout(() => {
-        window.location.href = 'views/menu.html'; // Cambia 'menu.html' por la URL de tu menú
-    }, 5000);
-
-});
