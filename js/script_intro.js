1f5cee99d49cd14da8d611f0036cac30da08d70c
ubikatcdmx <ubikatcdmx@gmail.com>
2025-12-11 10:52:57 -0600

    Add files via upload

---
+++ js/script_intro.js
@@ -0,0 +1,7 @@
+window.scrollTo(0, 1);
+document.addEventListener('DOMContentLoaded', () => {
+    // Redirige después de 5 segundos (5000 milisegundos)
+    setTimeout(() => {
+        window.location.href = 'views/menu.html'; // Cambia 'menu.html' por la URL de tu menú
+    }, 5000);
+});
